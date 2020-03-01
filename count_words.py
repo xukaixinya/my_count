4ad759dc37002ca8ea733e3a26b71c98a2460cf6
xuxin <550638677@qq.com>
2020-03-01 16:32:21 +0800

    commit

---
+++ count_words.py
@@ -0,0 +1,20 @@
+import re
+
+
+class Counter:
+    def __init__(self, path):
+        self._mapping = dict()
+        with open(path) as f:
+            data = f.read()
+            words = [s.lower() for s in re.findall("\w+", data)]
+            for word in words:
+                self._mapping[word] = self._mapping.get(word, 0) + 1
+
+    def most_common(self, n):
+        return sorted(self._mapping.items(), key=lambda item: item[1], reverse=True)[:n]
+
+
+if __name__ == '__main__':
+    most_common_3 = Counter(r"C:\pytest\test.txt").most_common(3)
+    for item in most_common_3:
+        print(item)
